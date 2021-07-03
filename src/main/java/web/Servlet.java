2b2385f4db6bd1a50d8e690d812bdfb5b3453449
cbustamante777duoc <cra.bustamante@alumnos.duoc.cl>
2021-07-02 23:42:36 -0400

    proyecto parametros lesion 2

---
+++ src/main/java/web/Servlet.java
@@ -0,0 +1,37 @@
+
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+    
+    @Override
+    protected void doPost(HttpServletRequest request,HttpServletResponse response ) throws IOException{
+        //leer los paramentros de formulario 
+        String usuario = request.getParameter("usuario");
+        String password = request.getParameter("password");
+        
+        //repuesta enviada al servidor de glassfish
+        System.out.println("usuario = " + usuario);
+        System.out.println("password = " + password);
+        
+        //respuesta enviada
+        PrintWriter out = response.getWriter();
+        out.print("<html>");
+        out.print("<body>");
+        out.print("El parametro usuario es :"+usuario);
+        out.print("<br/>");
+        out.print("El parametro password es :"+password);
+        out.print("</body>");
+        out.print("</html>");
+        out.close();
+    }
+}
